d9a5623830a3c22a70b94b73ad7b7da6f2bf3a75
DavideCapuozzo <devsoloweb@gmail.com>
2024-12-10 22:35:32 +0100

    installation all dependencies, tool and configuration

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+require('dotenv').config();// Carica le variabili dal file .env
 const express = require('express')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser'); //Un middleware che analizza i cookie inviati dal client al server.
 const cors = require('cors'); //Un middleware che gestisce il Cross-Origin Resource Sharing, permettendo ad applicazioni in altri domini di comunicare con il server.
 
-// create a database connection
-// create a separate file for this and then import/use that
+
+const URI_MONGO = process.env.URI_MONGO; // Usa la variabile di ambiente
 
 mongoose.connect(URI_MONGO).then(()=>console.log('MongoDB Connected')).catch((error)=> console.log(error));
 
@@ -12,5 +13,21 @@ const app = express()
 const PORT = process.env.PORT || 5000;
 
 app.use( //Metodo per aggiungere middleware all'applicazione
-    cors({}) //Aggiunge il supporto per le richieste Cross-Origin. L'oggetto vuoto {} indica che tutte le origini sono permesse.
+    cors({
+        origin : 'http://localhost:5173/',
+        methods : ['GET', 'POST', 'DELATE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            "Auttorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials : true
+    }) //Aggiunge il supporto per le richieste Cross-Origin. L'oggetto vuoto {} indica che tutte le origini sono permesse.
 )
+
+app.use(cookieParser);
+app.use(express.json());
+
+app.listen(PORT, ()=> console.log(`Server is now running on port ${PORT}`))
