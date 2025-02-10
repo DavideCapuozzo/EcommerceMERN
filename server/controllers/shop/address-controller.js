d2d1c52c6e495097454906b543e6408b0f7cdcbe
DavideCapuozzo <devsoloweb@gmail.com>
2025-02-10 21:28:14 +0100

    missing the delate address-controller

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,5 +1,27 @@
+const Address = require('../../models/Address')
+
+
 const addAddress = async(req, res) => {
     try{
+        const {userId, address, city, pincode, phone, notes} = req.body;
+
+        if( !userId || !address || !city || !pincode || !phone || !notes ){
+            return res.status(400).json({
+                success : false,
+                message : 'Invalid data provided!'
+            })
+        }
+
+        const newlyCreateAddress = new Address({
+            userId, address, city, pincode, phone, notes
+        })
+
+        await newlyCreateAddress.save();
+
+        res.status(201).json({
+            success : true,
+            data : newlyCreateAddress
+        })
 
     }catch (e) {
         console.log(e);
@@ -14,6 +36,22 @@ const addAddress = async(req, res) => {
 const fetchAllAddress = async(req, res) => {
     try{
 
+        const {userId} = req.params;
+
+        if( !userId ){
+            return res.status(400).json({
+                success : false,
+                message : 'User Id is required!'
+            })
+        }
+
+        const addressList = await Address.find({userId})
+
+        res.status(201).json({
+            success : true,
+            data : addressList
+        })
+
     }catch (e) {
         console.log(e);
         res.status(500).json({
@@ -26,6 +64,32 @@ const fetchAllAddress = async(req, res) => {
 
 const editAddress = async(req, res) => {
     try{
+
+        const { userId, addressId } = req.params;
+        const formData = req.body;
+
+        if( !userId || !addressId ){
+            return res.status(400).json({
+                success : false,
+                message : 'User Id and address Id is required!'
+            })
+        }
+
+        const address = await Address.findOneAndUpdate({
+            _id: addressId, userId
+        }, formData, {new: true})
+
+        if(!address){
+            return res.status(400).json({
+                success : false,
+                message : 'Address not found!',
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            data: address,
+        })
 
     }catch (e) {
         console.log(e);
