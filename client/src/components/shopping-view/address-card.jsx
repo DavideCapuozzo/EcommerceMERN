ab2685977f5dbc9fb4d3390aa7f150df582a002e
DavideCapuozzo <devsoloweb@gmail.com>
2025-02-18 22:27:30 +0100

    address edit and delete button in frontend

--- client/src/components/shopping-view/address-card.jsx
+++ client/src/components/shopping-view/address-card.jsx
@@ -1,16 +1,21 @@
-import { Card, CardContent, CardHeader } from "../ui/card"
+import { Button } from "../ui/button"
+import { Card, CardContent, CardFooter, CardHeader } from "../ui/card"
 import { Label } from "../ui/label"
 
-function AddressCard({addressInfo}) {
+function AddressCard({addressInfo, handleDeleteAddress, handleEditAddress}) {
     return(
         <Card>
-            <CardContent className="grid gap-4">
-                <Label>{addressInfo?.address}</Label>
-                <Label>{addressInfo?.city}</Label>
-                <Label>{addressInfo?.pincode}</Label>
-                <Label>{addressInfo?.phone}</Label>
-                <Label>{addressInfo?.notes}</Label>
+            <CardContent className="grid p-4 gap-4">
+                <Label>Address: {addressInfo?.address}</Label>
+                <Label>City: {addressInfo?.city}</Label>
+                <Label>Pincode: {addressInfo?.pincode}</Label>
+                <Label>Phone: {addressInfo?.phone}</Label>
+                <Label>Notes: {addressInfo?.notes}</Label>
             </CardContent>
+            <CardFooter className="flex p-3 justify-between">
+                <Button onClick={()=> handleEditAddress(addressInfo)}>Edit</Button>
+                <Button onClick={()=> handleDeleteAddress(addressInfo)}>Delate</Button>
+            </CardFooter>
         </Card>
     )
 }
--- client/src/components/shopping-view/address.jsx
+++ client/src/components/shopping-view/address.jsx
@@ -3,7 +3,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { useEffect, useState } from "react";
 import CommonForm from "../common/form";
 import { useDispatch, useSelector } from "react-redux";
-import { addNewAddress, fetchAllAddresses } from "@/store/shop/address-slice";
+import { addNewAddress, deleteAddress, editaAddress, fetchAllAddresses } from "@/store/shop/address-slice";
+import AddressCard from "./address-card";
+import { useToast } from "@/hooks/use-toast";
 
 const initialAddressFormData ={
     address : '',
@@ -17,21 +19,74 @@ const initialAddressFormData ={
 function Address() {
 
     const [formData, setFormData] = useState(initialAddressFormData)
+    const [currentEditId, setCurrentEditId] = useState(null)
     const dispatch = useDispatch()
     const {user} = useSelector(state => state.auth)
     const {addressList} = useSelector(state => state.shopAddress)
+    const { toast } = useToast()
 
     function handleManageAddress(event){
         event.preventDefault();
 
-        dispatch(addNewAddress({
+        if(addressList.length >= 3 && currentEditId === null){
+                setFormData(initialAddressFormData)
+            toast({
+                title: "You can add max 3 addresses",
+                variant: "destructive"
+            })
+
+            return;
+        }
+
+        currentEditId !== null ? 
+        
+        dispatch(editaAddress({
+            userId: user?.id, 
+            addressId: currentEditId, 
+            formData
+        })).then((data) => {
+            if(data?.payload?.success){
+                dispatch(fetchAllAddresses(user?.id));
+                setCurrentEditId(null);
+                setFormData(initialAddressFormData);
+                toast({
+                    title: "Address Edited",
+                })
+            }
+        }) : dispatch(addNewAddress({
             ...formData,
             userId: user?.id
         })).then(data =>{
             if(data?.payload?.success){
-                dispatch(fetchAllAddresses(user?.id))
-                setFormData(initialAddressFormData)
+                dispatch(fetchAllAddresses(user?.id));
+                setFormData(initialAddressFormData);
+                toast({
+                    title: "Address Added Succesfully",
+                })
             }
+        })
+    }
+
+    function handleDeleteAddress(getCurrentAddress){
+        dispatch(deleteAddress({userId : user?.id, addressId:getCurrentAddress._id})).then(data => {
+            if(data?.payload?.success){
+                dispatch(fetchAllAddresses(user?.id))
+                toast({
+                    title: "Address Deleted",
+                })
+            }
+        })
+    }
+
+    function handleEditAddress(getCurrentAddress){
+        setCurrentEditId(getCurrentAddress?._id)
+        setFormData({
+            ...formData,
+            address : getCurrentAddress?.address,
+            city : getCurrentAddress?.city,
+            phone : getCurrentAddress?.phone,
+            pincode : getCurrentAddress?.pincode,
+            notes : getCurrentAddress?.notes,
         })
     }
 
@@ -47,14 +102,16 @@ function Address() {
 
     return(
         <Card>
-            <div>
-                Address List
+            <div className="mb-5 p-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+                {
+                    addressList && addressList.length > 0 ? addressList.map(singleAddressItem => <AddressCard handleDeleteAddress={handleDeleteAddress} addressInfo={singleAddressItem} handleEditAddress={handleEditAddress}></AddressCard>) : null
+                }
             </div>
             <CardHeader>
-                <CardTitle>Add new Address</CardTitle>
+                <CardTitle>{currentEditId !== null? 'Edit Address' : 'Add New Address'}</CardTitle>
             </CardHeader>
             <CardContent className="space-y-3">
-                <CommonForm formControls={addressFormControls} formData={formData} setFormData={setFormData} buttontext={'Add'} onSubmit={handleManageAddress} isBtnDisabled={!isFormValid()}/>
+                <CommonForm formControls={addressFormControls} formData={formData} setFormData={setFormData} buttonText={currentEditId !== null? 'Edit' : 'Add'} onSubmit={handleManageAddress} isBtnDisabled={!isFormValid()}/>
             </CardContent>
         </Card>
     )
